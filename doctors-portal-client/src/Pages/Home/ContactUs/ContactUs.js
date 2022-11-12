672a1253b525a4e8f8e0de6effabbf66d25dc2ea
MORSHED-OFFICE <morshed952640@gmail.com>
2022-11-12 11:23:46 +0600

    Contact section added

---
+++ doctors-portal-client/src/Pages/Home/ContactUs/ContactUs.js
@@ -0,0 +1,35 @@
+import React from "react";
+import appointment from "../../../assets/images/appointment.png";
+
+const ContactUs = () => {
+  return (
+    <section className="py-32" style={{ background: `url(${appointment})` }}>
+      <div>
+        <div className="mb-10 text-center">
+          <h5 className="text-xl text-primary font-bold">Contact Us</h5>
+          <h2 className="text-3xl lg:text-4xl text-white">
+            Stay connected with us
+          </h2>
+        </div>
+      </div>
+      <form className="w-1/3 mx-auto">
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="input input-bordered input-md w-full"
+        />
+        <input
+          type="text"
+          placeholder="Subject"
+          className="input input-bordered input-md w-full my-3"
+        />
+        <textarea
+          className="textarea w-full"
+          placeholder="Your Meassage..."
+        ></textarea>
+      </form>
+    </section>
+  );
+};
+
+export default ContactUs;
--- doctors-portal-client/src/Pages/Home/Testomonials/Testimonials.js
+++ doctors-portal-client/src/Pages/Home/Testomonials/Testimonials.js
@@ -3,6 +3,7 @@ import TestimonialCard from "./TestimonialCard";
 import people1 from "../../../assets/images/people1.png";
 import people2 from "../../../assets/images/people2.png";
 import people3 from "../../../assets/images/people3.png";
+import quote from "../../../assets/icons/quote.svg";
 
 const Testimonials = () => {
   const TestimonialData = [
@@ -33,9 +34,14 @@ const Testimonials = () => {
   ];
   return (
     <div className="my-20">
-      <div className="my-4">
-        <h5 className="text-xl text-primary font-bold">Testimonial</h5>
-        <h2 className="text-accent text-4xl">What Our Patients Says</h2>
+      <div className="flex justify-between items-center my-10">
+        <div className="mb-10">
+          <h5 className="text-xl text-primary font-bold">Testimonial</h5>
+          <h2 className="text-accent  text-3xl lg:text-4xl">What Our Patients Says</h2>
+        </div>
+        <div>
+          <img src={quote} className="w-24 lg:w-48" alt="" />
+        </div>
       </div>
       <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
         {TestimonialData.map((testimonial) => (
