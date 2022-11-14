7e0cb290dcc4aaed36788950943053e9fdfaff17
MORSHED-OFFICE <morshed952640@gmail.com>
2022-11-14 12:37:08 +0600

    Update user successfully

--- doctors-portal-client/src/Pages/Dashboard/Dashboard/Dashboard.js
+++ doctors-portal-client/src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthProvider";
 import Navbar from "../../Shared/Navbar/Navbar";
 
 const Dashboard = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user);
   return (
     <div>
       <Navbar></Navbar>
-      <h2>Dashboard comming soooon...</h2>
+      <h2> {user?.displayName} Dashboard comming soooon...</h2>
     </div>
   );
 };
